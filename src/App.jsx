f2fa489a0b04338c0362470581e5e02d8529153c
Ennyola4 <Ennyola45@gmail.com>
2024-08-14 00:43:16 +0100

    Home page completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
